f22c145884704078ebe2360c6f26a75e89793cc6
Said Maadan <saidmaadan@users.noreply.github.com>
2017-02-15 21:31:27 -0600

    Storing and fetching date to and from database

--- routes/app.js
+++ routes/app.js
@@ -1,8 +1,26 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
 router.get('/', function (req, res, next) {
-    res.render('index');
+  User.findOne({}, function(err, data){
+    if(err){
+      return res.send('Error');
+    }
+    res.render('node', {email: data.email});
+  });
 });
+
+router.post('/', function(req, res, next){
+  var email = req.body.email;
+  var user = new User({
+    firstName: "Said",
+    lastName: "Maadan",
+    password: "secret123",
+    email: email
+  });
+  user.save();
+  res.redirect('/');
+})
 
 module.exports = router;
